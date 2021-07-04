523cff9aea02c828dd9e1b5bcb2c3bc55d8c8f9b
wjdwl002 <wjdwl001@khu.ac.kr>
2021-07-05 00:13:37 +0900

    1차본 완성

--- Bouncing-Ball/app.js
+++ Bouncing-Ball/app.js
@@ -5,12 +5,14 @@ import {
 class App{
     constructor(){
         this.canvas = document.createElement('canvas');
-        this.cts = this.canvas.getContext('2d'); //캔버스의 드로잉 컨텍스트 반환 - 2d : 2차원 렌더링 컨텍스트(CanvasRenderingContext2D)
+        this.ctx = this.canvas.getContext('2d'); //캔버스의 드로잉 컨텍스트 반환 - 2d : 2차원 렌더링 컨텍스트(CanvasRenderingContext2D)
 
         document.body.appendChild(this.canvas);
 
-        window.addEventListener('resize',this.resize(this),false);
+        window.addEventListener('resize',this.resize.bind(this),false);
         this.resize();
+
+        this.ball = new Ball(this.stageWidth,this.stageHeight,60,15)
 
         window.requestAnimationFrame(this.animate.bind(this));
     }
@@ -21,11 +23,13 @@ class App{
 
         this.canvas.width = this.stageWidth *2;
         this.canvas.height = this.stageHeight *2;
-        this.cts.scale(2,2);
+        this.ctx.scale(2,2);
     }
 
     animate(t){
         window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
     }
 }
 
